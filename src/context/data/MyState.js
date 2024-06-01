5c5294b8001ea7b8ebf42f9db0645b90f30c213b
Vaibhav <Vaibhavmathur128@gmail.com>
2024-06-01 10:36:41 +0530

    Adding Navbar, footer, and filter

--- src/context/data/MyState.js
+++ src/context/data/MyState.js
@@ -1,13 +1,20 @@
-import React from "react";
+import React, { useState } from "react";
 import MyContext from "./MyContext";
-function MyState(props){
- 
 
-    return(
-<MyContext.Provider >
-    {props.children}
-</MyContext.Provider>
-    )
+function MyState(props) {
+  const [mode, setMode] = useState("light");
 
+  const toggleMode = () => { // Correct the function name to toggleMode
+    if (mode === "light") {
+      setMode("dark");
+      document.body.style.backgroundColor = "rgb(17,24,39)"; // Fix typo
+    } else {
+      setMode("light");
+      document.body.style.backgroundColor = "white"; // Fix typo
+    }
+  };
+
+  return <MyContext.Provider value={{ mode, toggleMode }}>{props.children}</MyContext.Provider>;
 }
-export default MyState
+
+export default MyState;
--- src/pages/home/Homepage.jsx
+++ src/pages/home/Homepage.jsx
@@ -1,11 +1,20 @@
 import React, { useContext } from "react";
 import Layout from "../../components/layout/Layout"
 import MyContext from "../../context/data/MyContext";
+import HeroSection from "../../components/heroSection/HeroSection";
+import Filter from "../../components/filter/Filter"
+import ProductCard from "../../components/productCard/ProductCard";
 function Homepage() {
   const context=useContext(MyContext)
   console.log(context);
   const {name}=context;
-  return <Layout>Name:{name}</Layout>;
+  return (
+<Layout>
+    <HeroSection/>
+    <Filter/>
+    <ProductCard/>
+  </Layout>
+  )
 }
 
 export default Homepage;
